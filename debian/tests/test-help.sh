8e78bd69b15e4fd083c41cca083e2e7e0493a734
Raphaël Hertzog <hertzog@debian.org>
2017-06-23 15:41:17 +0200

    Improve debian/tests/test-help.sh.

--- debian/tests/test-help.sh
+++ debian/tests/test-help.sh
@@ -1,18 +1,14 @@
-#!/bin/sh -e
-##
-## test-help.sh
-##
-## Made by phil
-##
-##
-## Started on  Fri 23 Jun 2017 09:05:14 AM CEST pret
-## Last update Fri 23 Jun 2017 09:13:50 AM CEST pret
-##
+#!/bin/sh
 
-# test that the help (--h, --help) message is no more "None"
-helpsize=$(dhcpig --help 2>/dev/null|wc -l)
+set -e
 
-if test "$helpsize" != "42"
-then
-  exit 1
+# Ensure --help works
+dhcpig --help >$AUTOPKGTEST_TMP/output
+
+# Ensure --help prints something useful
+if ! grep -q "http://github.com/kamorin/DHCPig" $AUTOPKGTEST_TMP/output
+then 
+    echo "ERROR: dhcpig --help looks wrong (no URL output):"
+    cat $AUTOPKGTEST_TMP/output
+    exit 1
 fi
